7e102414321b4db60f186b44cb5a1b04be91096d
matthew-schultz <mschultz@hawaii.edu>
2018-05-09 19:15:52 -1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #sudo add-apt-repository ppa:webupd8team/java
 #sudo apt-get update
 #echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-echo "deb http://www.apache.org/dist/cassandra/debian 22x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
-echo "deb-src http://www.apache.org/dist/cassandra/debian 22x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
-gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
-gpg --export --armor F758CE318D77295D | sudo apt-key add -
-gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00
-gpg --export --armor 2B5C1B00 | sudo apt-key add -
-gpg --keyserver pgp.mit.edu --recv-keys 0353B12C
-gpg --export --armor 0353B12C | sudo apt-key add -
+#echo "deb http://www.apache.org/dist/cassandra/debian 22x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+echo "deb http://www.apache.org/dist/cassandra/debian 36x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
+#echo "deb-src http://www.apache.org/dist/cassandra/debian 22x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+#gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
+#gpg --export --armor F758CE318D77295D | sudo apt-key add -
+#gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00
+#gpg --export --armor 2B5C1B00 | sudo apt-key add -
+#gpg --keyserver pgp.mit.edu --recv-keys 0353B12C
+#gpg --export --armor 0353B12C | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install dirmngr --install-recommends
 sudo apt-get install -y cassandra
